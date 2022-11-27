3887fd380bdd2d8f7bd0a6f5415c2dbc1cee64a9
John Roller <jtroller33@gmail.com>
2022-11-27 00:45:36 -0600

    added react-bootstrap and navbar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+
 
 function App() {
   return (
-    <div className='container'>
-      
-      <div className='headerDiv'>
-        <div>
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-        <div className='h1Div'>
-          <h1 className='header'>ReactJS  Barcode Generator</h1>
-        </div>
-      </div>
+    <Navbar bg="primary" variant="dark">
+        <Container>
+          <div><img src={logo} className="App-logo" alt="logo" /></div>
+          <Navbar.Brand href="#navbar">ReactJS Barcode Generator</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#link1">link1</Nav.Link>
+            <Nav.Link href="#link2">link2</Nav.Link>
+            <Nav.Link href="#link3">link3</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
-      <div className='button1'>
-      <p>Button 1</p>
-      </div>
-
-      <div className='button2'>
-        <p>Button 2</p>
-      </div>
-
-
-    </div>
   );
 }
 
 export default App;
+
+/* <div className='container'>
+      
+<div className='headerDiv'>
+  <div>
+    <img src={logo} className="App-logo" alt="logo" />
+  </div>
+  <div className='h1Div'>
+    <h1 className='header'>ReactJS  Barcode Generator</h1>
+  </div>
+</div>
+
+<div className='button1'>
+<p>Button 1</p>
+</div>
+
+<div className='button2'>
+  <p>Button 2</p>
+</div>
+
+
+</div> */
 
 
 // function App() {
