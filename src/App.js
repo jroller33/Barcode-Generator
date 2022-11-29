9dc1771eefdd534c203a55f2757915e04a8ce17b
John Roller <jtroller33@gmail.com>
2022-11-29 13:56:15 -0600

    working on layout with barcode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React, { useState } from 'react';
-import JsBarcode from 'jsbarcode';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-var ReactDOM = require('react-dom');
-var Barcode = require('react-barcode');
+import { useBarcode } from 'react-barcode';
+
+
+function App() {
+  const inputRef = console.log(useBarcode({
+    value: 'hello'
+  }));
+
+
+  return (
+    <div className='container py-4'>
+      <div>
+      <div><img src={logo} className="App-logo" alt="logo" /></div>
+
+      <h1 className='display-5 fw-bold'>ReactJS Barcode Generator</h1>
+      </div>
+      <div>
+        <svg ref={inputRef} />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
 // import React, { useState } from 'react';
 // import Jumbotron from 'react-bootstrap/Jumbotron';
 // import Toast from 'react-bootstrap/Toast';
@@ -15,40 +37,19 @@ var Barcode = require('react-barcode');
 // const JsBarcode = require('jsbarcode');
 // const { Canvas } = require("canvas");
 
-// have separate pages for backroom barcodes, UPCs, DPCIs etc linked on navbar
-
-function App() {
-
-  return (
-    <div className='container py-4'>
-      <div>
-      <div><img src={logo} className="App-logo" alt="logo" /></div>
-
-      <h1 className='display-5 fw-bold'>ReactJS Barcode Generator</h1>
-      </div>
-      <div>
-        <Barcode value="hello" />
-      </div>
-
-    </div>
-  );
-}
-
-export default App;
-
 /*  OLD HOMEPAGE    */
 // import Navbar from 'react-bootstrap/Navbar';
 // import Nav from 'react-bootstrap/Nav';
 
 /* <Navbar bg="primary" variant="dark">
 <Container>
-  <div><img src={logo} className="App-logo" alt="logo" /></div>
-  <Navbar.Brand href="#navbar">ReactJS Barcode Generator</Navbar.Brand>
-  <Nav className="me-auto">
-    <Nav.Link href="#link1">link1</Nav.Link>
-    <Nav.Link href="#link2">link2</Nav.Link>
-    <Nav.Link href="#link3">link3</Nav.Link>
-  </Nav>
+<div><img src={logo} className="App-logo" alt="logo" /></div>
+<Navbar.Brand href="#navbar">ReactJS Barcode Generator</Navbar.Brand>
+<Nav className="me-auto">
+<Nav.Link href="#link1">link1</Nav.Link>
+<Nav.Link href="#link2">link2</Nav.Link>
+<Nav.Link href="#link3">link3</Nav.Link>
+</Nav>
 </Container>
 </Navbar> */
 
@@ -76,12 +77,12 @@ export default App;
 /* <div className='container'>
 
 <div className='headerDiv'>
-  <div>
-    <img src={logo} className="App-logo" alt="logo" />
-  </div>
-  <div className='h1Div'>
-    <h1 className='header'>ReactJS  Barcode Generator</h1>
-  </div>
+<div>
+<img src={logo} className="App-logo" alt="logo" />
+</div>
+<div className='h1Div'>
+<h1 className='header'>ReactJS  Barcode Generator</h1>
+</div>
 </div>
 
 <div className='button1'>
@@ -89,7 +90,7 @@ export default App;
 </div>
 
 <div className='button2'>
-  <p>Button 2</p>
+<p>Button 2</p>
 </div>
 
 
@@ -97,13 +98,13 @@ export default App;
 
 
 // function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
+  //   return (
+    //     <div className="App">
+    //       <header className="App-header">
+    //         <img src={logo} className="App-logo" alt="logo" />
+    //         <p>
+    //           Edit <code>src/App.js</code> and save to reload.
+    //         </p>
 //         <a
 //           className="App-link"
 //           href="https://reactjs.org"
