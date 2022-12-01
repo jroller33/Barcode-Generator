b218d519d627595f74c5230b4b906d419bd9dfa6
John Roller <jtroller33@gmail.com>
2022-11-30 22:25:02 -0600

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './App.css';
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
